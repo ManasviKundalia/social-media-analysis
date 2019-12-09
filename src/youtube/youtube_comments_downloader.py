9074bd84d58a423629063b7e4dadd87ea03d859f
Manasvi Kundalia <kmanasvi.paresh.eee13@iitbhu.ac.in>
2019-12-09 10:25:35 +0530

    youtube comments downloader -  get replies from comment threads

--- src/youtube/youtube_comments_downloader.py
+++ src/youtube/youtube_comments_downloader.py
@@ -1,5 +1,6 @@
 import csv
 import configparser
+import pickle as pk
 
 from youtube.youtube_client import get_authenticated_service
 
@@ -50,36 +51,72 @@ class YouTubeCommentsDownloader:
         # for item in results['items']:
         #     print('%s - %s' % (item['snippet']['title'], item['id']['videoId']))
         final_result = []
-        for item in results:
+        for item in results[2:]:
             title = item['snippet']['title']
             video_id = item['id']['videoId']
+            # print(title, video_id)
             comments = self.get_video_comments(service, part='snippet', videoId=video_id, textFormat='plainText')
             final_result.extend([(video_id, title, comment) for comment in comments])
-        return results
+
+        return final_result
 
     def get_video_comments(self,service, **kwargs):
         comments = []
         results = service.commentThreads().list(**kwargs).execute()
-
+        # print(results)
+        parent_ids = []
         while results:
             for item in results['items']:
-                comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
-                comments.append(comment)
-            # if True:
-            #     break
+                thread_id = item['id']
+                topLevelComment_snippet = item['snippet']['topLevelComment']['snippet']
+
+                comment_obj = {}
+                comment_obj['thread_id'] = thread_id
+                comment_obj['comment'] = topLevelComment_snippet['textDisplay']
+                comment_obj['author'] = topLevelComment_snippet['authorDisplayName']
+                comment_obj['authorChannelId'] = topLevelComment_snippet['authorChannelId']
+                comment_obj['videoId'] = topLevelComment_snippet['videoId']
+                comment_obj['canRate'] = topLevelComment_snippet['canRate']
+                comment_obj['viewerRating'] = topLevelComment_snippet['viewerRating']
+                comment_obj['likeCount'] = topLevelComment_snippet['likeCount']
+                # print(item)
+                comments.append(comment_obj)
+
+                try:
+                    replies = item['replies']['comments']
+
+                except:
+                    replies = []
+                if len(replies)!=item['snippet']['totalReplyCount']:
+                    parent_ids.append(thread_id)
+
+                for reply in replies:
+                    comment_obj = {}
+                    comment_obj['thread_id'] = thread_id
+                    comment_obj['comment'] = reply['snippet']['textDisplay']
+                    comment_obj['author'] = reply['snippet']['authorDisplayName']
+                    comment_obj['authorChannelId'] = reply['snippet']['authorChannelId']
+                    comment_obj['videoId'] = reply['snippet']['videoId']
+                    comment_obj['canRate'] = reply['snippet']['canRate']
+                    comment_obj['viewerRating'] = reply['snippet']['viewerRating']
+                    comment_obj['likeCount'] = reply['snippet']['likeCount']
+                    comments.append(comment_obj)
+
             if 'nextPageToken' in results:
                 kwargs['pageToken'] = results['nextPageToken']
                 results = service.commentThreads().list(**kwargs).execute()
             else:
                 break
-
+        # print(comments)
+        print(parent_ids)
         return comments
 
     def save_comments_data(self):
         if len(self.comments_data)==0:
             raise Warning("No data to save. Execute search_by_keywords to get data.")
             return
-        print(self.comments_data[self.keywords[0]])
+        # print(self.comments_data[self.keywords[0]])
+        pk.dump(self.comments_data, open('comments_data_'+'_'.join(self.keywords)+'.pkl', 'wb'))
         return
 
 
@@ -87,3 +124,4 @@ class YouTubeCommentsDownloader:
 if __name__=='__main__':
     yt_comments_downloader = YouTubeCommentsDownloader()
     yt_comments_downloader.search_by_keywords()
+    yt_comments_downloader.save_comments_data()
